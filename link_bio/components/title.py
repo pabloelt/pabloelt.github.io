849cf607f0ae56f0dfbf1c8bc0611c46e8ee6724
pabloelt <pe.lopeztello@gmail.com>
2024-03-12 21:21:13 +0100

    Initial commit

--- link_bio/components/title.py
+++
@@ -1,9 +0,0 @@
-import reflex as rx
-import link_bio.styles.styles as styles
-
-def title(text: str) -> rx.Component:
-    return rx.heading(
-        text,
-        size = "4",
-        style = styles.TITLE_STYLE,
-    )
